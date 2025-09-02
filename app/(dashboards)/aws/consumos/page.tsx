0c63c0d46de88c7ceafca746b286ba8261517f7a
JoseAl05 <jparancibialinker@gmail.com>
2025-09-02 15:04:03 -0400

    created consume page

--- app/(dashboards)/aws/consumos/page.tsx
+++ app/(dashboards)/aws/consumos/page.tsx
@@ -1,10 +1,12 @@
 import { MainViewConsume } from '@/components/aws/vista-consumos/MainViewConsume';
+import { Suspense } from 'react';
 
 export default function DashboardAwsConsumePage() {
-    return(
-        <div className='p-5'>
-            <h1 className=''>Dashboard AWS / Consumos</h1>
-            <MainViewConsume />
+    return (
+        <div className=''>
+            <Suspense fallback={<div>Cargando...</div>}>
+                <MainViewConsume />
+            </Suspense>
         </div>
     )
 }
