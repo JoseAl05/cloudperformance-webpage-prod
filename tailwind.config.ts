b6d6b7255a12780d42af290142120c18d2baa149
JoseAl05 <jparancibialinker@gmail.com>
2025-10-22 13:36:38 -0300

    changed configurations

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  safelist: [], // mantén esto corto
+  theme: { extend: {} },
+  plugins: [],
+};
+export default config;
