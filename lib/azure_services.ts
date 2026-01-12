7986cbf41f4c60931c7e74b91964efc2a524e607
JoseAl05 <jparancibialinker@gmail.com>
2026-01-12 09:58:08 -0300

    created variable for compute services

--- lib/azure_services.ts
+++ lib/azure_services.ts
@@ -1,12 +1,19 @@
 export const azure_services = [
-    { label: 'Virtual Machines', value: 'vm' },
-    { label: 'VM Scale Sets', value: 'vmss' },
-    { label: 'Nodos K8S', value: 'vmssk8s' },
-    { label: 'PostgreSQL Flexible', value: 'dbpg' },
-    { label: 'Mysql Flexible', value: 'dbmysql' },
-    { label: 'Storage Account', value: 'strgacc' },
-    { label: 'Blob Service', value: 'blobservice' },
-    { label: 'File Service', value: 'fileservice' },
-    { label: 'Queue Service', value: 'queueservice' },
-    { label: 'Table Service', value: 'tableservice' }
-]
+  { label: 'Virtual Machines', value: 'vm' },
+  { label: 'VM Scale Sets', value: 'vmss' },
+  { label: 'Nodos K8S', value: 'vmssk8s' },
+  { label: 'PostgreSQL Flexible', value: 'dbpg' },
+  { label: 'Mysql Flexible', value: 'dbmysql' },
+  { label: 'Storage Account', value: 'strgacc' },
+  { label: 'Blob Service', value: 'blobservice' },
+  { label: 'File Service', value: 'fileservice' },
+  { label: 'Queue Service', value: 'queueservice' },
+  { label: 'Table Service', value: 'tableservice' },
+];
+export const azure_compute_services = [
+  { label: 'Virtual Machines', value: 'vm' },
+  { label: 'VM Scale Sets', value: 'vmss' },
+  { label: 'Nodos K8S', value: 'vmssk8s' },
+  { label: 'PostgreSQL Flexible', value: 'dbpg' },
+  { label: 'Mysql Flexible', value: 'dbmysql' },
+];
