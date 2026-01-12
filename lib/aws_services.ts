57511c8093cb636963cd8a07eea7c708febfecd9
JoseAl05 <jparancibialinker@gmail.com>
2026-01-12 09:58:19 -0300

    created variable for compute services

---
+++ lib/aws_services.ts
@@ -0,0 +1,8 @@
+export const aws_compute_services = [
+  { label: 'Elastic Compute Cloud (EC2)', value: 'ec2' },
+  { label: 'RDS Mysql', value: 'mysql' },
+  { label: 'RDS Postgresql', value: 'postgresql' },
+  { label: 'RDS Oracle', value: 'oracle' },
+  { label: 'RDS Mariadb', value: 'mariadb' },
+  { label: 'RDS Sql Server', value: 'sqlserver' },
+];
