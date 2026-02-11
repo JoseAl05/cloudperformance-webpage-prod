f56ce157bf398346a432d9c62c6a88347c4d58fb
felipenanculef <102937232+felipenanculef@users.noreply.github.com>
2026-02-11 14:47:51 -0300

    avances gcp

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -287,12 +287,9 @@ export const SidebarComponent = ({
             // { label: 'Incremento Uso de Recursos', icon: LineChart, href: '/gcp/funciones/incremento-top-recursos-uso' },
             { label: 'Spot vs Standard VMs', icon: Computer, href: '/gcp/funciones/spot-vs-standard-vm' },
             { label: 'Promedio de uso por región', icon: MapPin, href: '/gcp/funciones/uso-costo-por-localizacion' },
-<<<<<<< HEAD
             { label: 'Top Facturación por Región', icon: DollarSign, href: '/gcp/funciones/top-facturacion-region' },
             { label: 'Consumo horario hábil vs no hábil', icon: Clock, href: '/gcp/funciones/analisis-vms-horario' },
-=======
             { label: 'Consumo horario hábil vs no hábil', subItems: workingNonWorkingItemsGCP, icon: Clock },
->>>>>>> 9aabf45929508d01b11e37ad91287b54ca62f4eb
             // { label: 'Promedio de uso por región', icon: MapPin, href: '/gcp/funciones/promedio-por-localizacion' },
             // { label: 'Consumo horario hábil vs no hábil', icon: Clock, href: '/gcp/funciones/analisis-vms-horario' },
             { label: 'Recursos no utilizados', subItems: infrausedGCP, icon: TrendingDown }
